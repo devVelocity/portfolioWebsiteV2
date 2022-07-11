a93cd6238410799ca343915f2b48c27609d15cad
Kieran <88147999+devVelocity@users.noreply.github.com>
2022-07-11 01:16:54 +0100

    working on images

--- src/components/imgwindow.vue
+++ src/components/imgwindow.vue
@@ -1,8 +1,10 @@
 <template>
     <div v-if="open" @click.self="closeWindow" class="bg-black w-full bg-opacity-80 h-full fixed top-0 z-40">
-        <div class="bg-black h-20 z-40 flex items-center xl:pl-[12%] xl:pr-[12%] pl-4 pr-4 lg:pr-8 justify-between fixed top-0 w-full"></div>
-        <div class="top-20 fixed z-40 w-[95%] h-[90%] left-[2.5%]">
-
+        <div class="bg-black h-20 z-40 flex items-center xl:pl-[12%] xl:pr-[12%] pl-4 pr-4 lg:pr-8 justify-between fixed top-0 w-full">
+        </div>
+        <div class="top-20 fixed z-40 w-[95%] h-[90%] left-[2.5%] flex">
+            
+            <img :src="require(`@/assets/images/`+imgsrc[0])">
         </div>
     </div>
     <!-- <div v-if="open" class="bg-black w-full bg-opacity-80 h-full fixed top-0 z-40">
@@ -16,7 +18,6 @@
         props:['open','imgsrc','imgalt'],
         data(){
             return{
-
                 
             }
         },
